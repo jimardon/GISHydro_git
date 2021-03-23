243786f2f55cfd7dd9d5faf85297f439d9d3faf2
jimardon <jimardon@gmail.com>
2021-03-23 16:52:29 -0400

    commit1

--- js/gishydrotools.js
+++ js/gishydrotools.js
@@ -286,7 +286,10 @@ function delineate(){
         document.getElementById('pp_lat').value = mark_lat
     }
     map.panTo(new L.LatLng(mark_lat, mark_lon));
-
+    alert(full_project_name)
+    alert(mark_lat)
+    alert(mark_lon)
+    alert(delcheckin)
     gpTask.setParam("projectname", full_project_name);
     gpTask.setParam("mouse_lat_proj", mark_lat);
     gpTask.setParam("mouse_lon_proj", mark_lon);
