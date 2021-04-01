ca0069d7a12444e8f69b644493e4c2f93218583f
jimardon <jimardon@gmail.com>
2021-04-01 10:44:25 -0400

    commit1

--- js/combineseg.js
+++ js/combineseg.js
@@ -11,20 +11,19 @@ function combinesegments(subarea,type, elev, slope, avgarea, width, depth, xarea
         var pixelmerge = 1
         var elevmerge = parseFloat(arrAvg(elev.map(Number))).toFixed(2)
         var slopemerge = parseFloat(arrAvg(slope.map(Number))).toFixed(2)
-        var avgareamerge = arrSum(avgarea.map(Number))
-        var i_lengthmerge = arrSum(i_length.map(Number))
+        var avgareamerge = parseFloat(arrSum(avgarea.map(Number))).toFixed(2)
+        var i_lengthmerge = parseFloat(arrSum(i_length.map(Number))).toFixed(2)
 
         var sheetn = parseFloat(document.getElementById("sheet_manning").value)
         var sheetp = parseFloat(document.getElementById("sheet_precipitation").value)
         var sheetl = parseFloat(document.getElementById("sheet_length").value)
 
-        var i_timemerge = parseFloat(0.007*(sheetn*sheetl)**0.8/sheetp**0.5/slopemerge**0.4).toFixed(2)
+        var i_timemerge = parseFloat(Math.pow(0.007*(sheetn*sheetl),0.8)/Math.pow(sheetp,0.5)/Math.pow(slopemerge,0.4)).toFixed(2)
         var velmerge = parseFloat(i_lengthmerge/i_timemerge).toFixed(2)
 
         alert([subarea,pixelmerge,"overland",elevmerge,slopemerge,avgareamerge,i_lengthmerge,velmerge,i_timemerge,i_timemerge])
 
-        createtable(subarea,pixelmerge,"overland",elevmerge,slopemerge,avgareamerge,i_lengthmerge,velmerge,i_timemerge,i_timemerge)
-
+        createtable(subarea,pixelmerge,"overland",elevmerge,slopemerge,avgareamerge,"-1","-1","-1",i_lengthmerge,velmerge,i_timemerge,i_timemerge)
 
     } else if (type == 2){
 
