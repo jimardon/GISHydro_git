524b0cfc0464a319870cc391d8a31212d81bd34d
jimardon <jimardon@gmail.com>
2021-04-01 15:17:41 -0400

    commit1

--- js/gishydrotools.js
+++ js/gishydrotools.js
@@ -215,6 +215,7 @@ call (410) 767-4500.)","20vh")
         sidebar.open('shed_del');
         drawLayers.clearLayers();
         $('#infstr-button').removeAttr('disabled');
+        $('#landuse-button').removeAttr('disabled');
         map.spin(false);
     }
 }
