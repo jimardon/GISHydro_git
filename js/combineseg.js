c81151bb359097a10960e402fdd5c5bbea46c8fc
jimardon <jimardon@gmail.com>
2021-04-06 18:41:25 -0400

    commit1

--- js/combineseg.js
+++ js/combineseg.js
@@ -163,7 +163,7 @@ function recalculatetc(){
 
         const channeln = parseFloat(document.getElementById("channel_manning").value)
         //const hydrad = xareamerge[0]/(2*depthmerge[0]+widthmerge[0])
-        const hydrad = xareamerge[0]/(2*depthmerge[0])
+        const hydrad = xareamerge[0]/(2*widthmerge[0])
 
         const velmerge = [parseFloat(1.49*Math.pow(hydrad,2/3)*Math.pow(slopemerge[0],0.5)/channeln).toFixed(3)]
         const i_timemerge = [parseFloat(tot_lengthmerge[0]/velmerge[0]/3600).toFixed(4)]
