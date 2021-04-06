5853c6e447cada7b0dd448175cf760ab63abc2ea
jimardon <jimardon@gmail.com>
2021-04-06 19:09:24 -0400

    commit1

--- js/combineseg.js
+++ js/combineseg.js
@@ -162,18 +162,12 @@ function recalculatetc(){
         const tot_lengthmerge = [parseFloat(tot_length_channel[tot_length_channel.length - 1]).toFixed(1)]
 
         const channeln = parseFloat(document.getElementById("channel_manning").value)
-        //const hydrad = xareamerge[0]/(2*depthmerge[0]+widthmerge[0])
-        alert(widthmerge)
-        alert(depthmerge)
-        alert(xareamerge)
-        const hydrad = xareamerge[0]/(2*widthmerge[0])
-        const hydrad2 = 2*depthmerge[0]+widthmerge[0]
+        const hydrad = xareamerge[0]/(2*Number(depthmerge[0])+Number(widthmerge[0]))
 
         const velmerge = [parseFloat(1.49*Math.pow(hydrad,2/3)*Math.pow(slopemerge[0],0.5)/channeln).toFixed(3)]
         const i_timemerge = [parseFloat(tot_lengthmerge[0]/velmerge[0]/3600).toFixed(4)]
 
         alert(hydrad)
-        alert(hydrad2)
 
         t_temp[subarea] = type_sheet.concat(type_shallow).concat(typemerge)
         e_temp[subarea] = elev_sheet.concat(elev_shallow).concat(elevmerge)
